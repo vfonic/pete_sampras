7a11cc11574b774c4a8972ff25814b88c96c0ef1
Viktor Fonic <viktor.fonic@gmail.com>
2015-07-27 15:52:10 +0100

    Implement banner rotation logic; Display only one banner per request

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,12 +1,22 @@
 class CampaignsController < ApplicationController
-  before_action :set_campaign, only: :show
+  before_action :set_banner, only: :show
 
   def show
   end
 
   private
-    def set_campaign
+    def set_banner
       # campaign/banner selection logic here
-      @banners = Banner.top_banners(campaign: params[:campaign_id])
+      banner_ids = session[:banner_ids]
+
+      if banner_ids.blank?
+        banners = Banner.top_banners(campaign: params[:campaign_id])
+        banners.shuffle!
+        session[:banner_ids] = banners.map(&:id)
+        banner_ids = session[:banner_ids]
+      end
+
+      banner_id = banner_ids.shift
+      @banner = Banner.find(banner_id)
     end
 end
